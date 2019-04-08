3e10089225542d570f6068fd4ee4c6641b92f62a
Andrey Keske <hello@andreykeske.com>
2019-04-07 22:22:22 -0400

    Cube some new vars

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -14,11 +14,18 @@ type Props = {
   screen: Screen,
 };
 
+const getRnd = (min: number, max: number): number => (
+  Math.floor(Math.random() * (max - min + 1)) + min
+);
+
 export default class Cube extends React.Component<Props> {
 
   add = (scene: any) => {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
+    const size = 0.5;
+    const geometry = new THREE.BoxGeometry(size, size, size);
+    const material = new THREE.MeshBasicMaterial({
+      color: `rgb(${getRnd(0, 255)}, ${getRnd(0, 255)}, ${getRnd(0, 255)})`,
+    });
 
     this.object = new THREE.Mesh(geometry, material);
 
