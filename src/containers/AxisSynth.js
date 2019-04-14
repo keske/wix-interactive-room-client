5c627982e1bcdd3c073a2ff828a8dd2ba728d006
Andrey Keske <hello@andreykeske.com>
2019-04-13 22:46:09 -0400

    Testing with Objects

--- src/containers/AxisSynth.js
+++ src/containers/AxisSynth.js
@@ -21,19 +21,48 @@ export default (props: Props): React.Node => (
       {
         animate: {
           position: {
-            x: props.mouse.x / 150,
-            y: -(props.mouse.y / 150),
-            z: 0,
+            x: (props.mouse.x - (window.innerWidth / 2)) / 30,
+            y: (props.mouse.y - (window.innerHeight / 2)) / -30,
+            z: (props.mouse.x - props.mouse.y) / 30,
           },
         },
-        object: sphere(),
+        object: sphere({
+          size: 2.5,
+          color: '#000',
+        }),
       },
       {
         animate: {
           position: {
-            x: props.mouse.x / 50,
-            y: -(props.mouse.y / 50),
-            z: 0,
+            x: (props.mouse.x - (window.innerWidth / 1.3)) / 40,
+            y: (props.mouse.y - (window.innerHeight / 1.3)) / -40,
+            z: (props.mouse.x - props.mouse.y) / 40,
+          },
+        },
+        object: sphere({
+          size: 2.3,
+          color: '#000',
+        }),
+      },
+      {
+        animate: {
+          position: {
+            x: (props.mouse.x - (window.innerWidth / 2.3)) / 70,
+            y: (props.mouse.y - (window.innerHeight / 2.3)) / -70,
+            z: (props.mouse.x - props.mouse.y) / 70,
+          },
+        },
+        object: sphere({
+          size: 2.7,
+          color: '#000',
+        }),
+      },
+      {
+        animate: {
+          position: {
+            x: (props.mouse.x - (window.innerWidth / 2)) / 70,
+            y: (props.mouse.y - (window.innerHeight / 2)) / -70,
+            z: (props.mouse.x - props.mouse.y) / 50,
           },
           rotation: {
             x: props.mouse.x / 50,
@@ -41,7 +70,22 @@ export default (props: Props): React.Node => (
             z: 0,
           },
         },
-        object: cube({ size: 4 }),
+        object: cube({ size: 2 }),
+      },
+      {
+        animate: {
+          position: {
+            x: (props.mouse.x - (window.innerWidth / 2)) / 100,
+            y: (props.mouse.y - (window.innerHeight / 2)) / -100,
+            z: (props.mouse.x - props.mouse.y) / 50,
+          },
+          rotation: {
+            x: props.mouse.x / 20,
+            y: props.mouse.y / 20,
+            z: 0,
+          },
+        },
+        object: cube({ size: 3 }),
       },
     ]}
   />
