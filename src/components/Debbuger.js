1965b9613eb065be6e64df3c8c0588c6c065bad2
Andrey Keske <hello@andreykeske.com>
2019-04-30 00:16:20 -0400

    Tech Info

--- src/components/Debbuger.js
+++ src/components/Debbuger.js
@@ -4,43 +4,67 @@ import * as React from 'react';
 import * as R from 'ramda';
 
 // Types
-import type { Acceleration, MouseOrTouchPosition } from '../types';
+import type { MouseOrTouchPosition } from '../types';
 
 type Props = {
-  acceleration: Acceleration,
   mouse: MouseOrTouchPosition,
 };
 
-export default ({ acceleration, mouse }: Props): React.Node => (
+export default ({ mouse: { x, y } }: Props): React.Node => (
   R.equals(process.env.REACT_APP_STAGE, 'development') && (
     R.pipe(
       ({ styles }) => (
         <div style={styles.root}>
-          <span style={styles.text}>
+          <p style={styles.text}>
             {`
-              Acceleration 
-                X: ${acceleration.x}
-                Y: ${acceleration.y}
-                Z: ${acceleration.z}
-              Mouse
-                X: ${mouse.x}
-                Y: ${mouse.y}
+              Server
+              ip: 134.209.218.211
             `}
-          </span>
+          </p>
+          <p style={styles.text}>
+            {`
+              Pixel Ratio
+              ${window.devicePixelRatio}
+            `}
+          </p>
+          <p style={styles.text}>
+            {`
+              Screen
+              w: ${window.innerWidth} px / 
+              h: ${window.innerHeight} px
+            `}
+          </p>
+          <p style={styles.text}>
+            {`
+              Position
+              x: ${x} px /
+              y: ${y} px
+            `}
+          </p>
+          <p style={styles.text}>
+            {`
+              Delay
+              time: ${x / 1000} ms /
+              wet: ${y / 1000} ms /
+              feedback: ${(x / 2 + y / 2) / 1000} ms
+            `}
+          </p>
         </div>
       ),
     )({
       styles: {
         root: {
-          backgroundColor: 'white',
-          top: 0,
-          left: 0,
+          top: 20,
+          left: 20,
           padding: 7,
           position: 'absolute',
           zIndex: 2,
+
+          opacity: 0.3,
         },
         text: {
-          fontFamily: 'Helvetica',
+          color: '#FFF',
+          fontFamily: 'Monospace',
           fontSize: 10,
           fontWeight: 'bold',
         },
